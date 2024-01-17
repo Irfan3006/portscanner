b7ab1618ba1a73c750cb2d518b9c7fc6ae730160
Irfan Syarifudin <92904571+Irfan3006@users.noreply.github.com>
2024-01-17 11:14:53 +0700

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,26 @@
+import socket
+import termcolor
+
+
+def scan(target, ports):
+    print('\n' + ' Mulai Scan Untuk ' + str(target))
+    for port in range (1,ports):
+        scan_port(target,port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(termcolor.colored(("[+] Port Terbuka " + str(port)), 'red'))
+        sock.close()
+    except:
+        pass
+
+targets = input("[*] Masukkan Target Untuk Di Scan (pisahkan dengan , (192.168.xx.xx,192.167.xx.xx,192.166.xx.xx)): ")
+ports = int(input("[*] Masukkan Berapa Banyak Port Yang Akan Di Scan: "))
+if ',' in targets:
+        print(termcolor.colored(("[*] Scanning Beberapa Target Sekaligus"), 'green'))
+        for ip_addr in targets.split(','):
+            scan(ip_addr.strip(' '), ports)
+else:
+        scan(targets,ports)
